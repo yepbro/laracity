736de6c1eb17e003164583c407c4525b35574e7c
Denis Yepbro <denis@yepbro.ru>
2025-10-21 14:45:57 +0300

    Laravel 12

---
+++ config/telegram-logger.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    // Telegram logger bot token
+    'token' => env('TELEGRAM_LOGGER_BOT_TOKEN'),
+
+    // Telegram chat id
+    'chat_id' => env('TELEGRAM_LOGGER_CHAT_ID'),
+
+    // Telegram message thread id
+    'message_thread_id' => env('TELEGRAM_LOGGER_MESSAGE_THREAD_ID', null),
+
+    // Blade Template to use formatting logs
+    'template' => env('TELEGRAM_LOGGER_TEMPLATE', 'laravel-telegram-logging::standard'),
+
+    // Proxy server
+    'proxy' => env('TELEGRAM_LOGGER_PROXY', ''),
+
+    // Telegram API host without trailling slash
+    'api_host' => env('TELEGRAM_LOGGER_API_HOST', 'https://api.telegram.org'),
+
+    // Telegram sendMessage options: https://core.telegram.org/bots/api#sendmessage
+    'options' => [
+        // 'parse_mode' => 'html',
+        'disable_web_page_preview' => true,
+        // 'disable_notification' => false
+    ],
+];
